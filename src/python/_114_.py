3b0010609ba1c1aa955f6c2023132c144a9e4ef7
vksabesa <vigneshsab@gmail.com>
2021-06-19 23:06:58 -0400

    Python files added

---
+++ src/python/_114_.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        while root:
+            if root.left:
+                node = root.left
+                while node.right:
+                    node = node.right
+                node.right = root.right
+                root.right = root.left
+                root.left = None
+            root = root.right
