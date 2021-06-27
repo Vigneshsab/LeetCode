66d0753d851470ced9234bb3a394e10e1325b487
vksabesa <vigneshsab@gmail.com>
2021-06-26 23:41:55 -0400

    Python files added

---
+++ src/python/_344_.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        #s[:] = s[::-1] 
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
+        
